be4c0676a60e16462d0a61aaf240e47828d8301a
Emiliano Carlos de Moraes Firmino <elmiliox@gmail.com>
2010-07-18 18:16:30 -0400

    matriz colorida

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-var mapa = [
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-];
 var origem = {x: 10, y: 7};
 var destino = {x: 5, y:3};
+var mapa = [
+[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+[-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1],
+[-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1],
+[-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1],
+[-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1],
+[-1,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1],
+[-1,0,0,0,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,-1],
+[-1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1],
+[-1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1],
+[-1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,-1],
+[-1,0,0,0,1,2,1,0,0,0,0,0,0,0,0,0,0,0,0,-1],
+[-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1],
+[-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1],
+[-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1],
+[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+];
 function getChoice(){
 	var choices = document.getElementById("choice").algorithm;
 	for(var i = 0; i< choices.length; i++){
@@ -28,8 +28,6 @@ function getChoice(){
 function runPathFinding(){
 	var option = getChoice();
 	graphic.render();
-	graphic.drawPoint(origem.x, origem.y, "O");
-	graphic.drawPoint(destino.x, destino.y, "X");
 }
 function Graphic(canvas, linesX, linesY){
 	this.canvas = canvas;
@@ -41,6 +39,14 @@ function Graphic(canvas, linesX, linesY){
 }
 Graphic.prototype.render = function () {
 	this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
+	var cor;
+	for(var i = 0; i < graph.grid.length; i++){
+		for(var j = 0; j < graph.grid[i].length; j++){
+			graphic.drawBox(j, i, graph.grid[i][j].cor);
+		}
+	}
+	graphic.drawBox(origem.x, origem.y, "rgba(255,0,0,0.5)");
+	graphic.drawBox(destino.x, destino.y, "rgba(0,0,255,0.5)");
 	this.drawGridLines();
 };
 Graphic.prototype.drawGridLines = function () {
@@ -55,7 +61,7 @@ Graphic.prototype.drawGridLines = function () {
 		this.context.lineTo(line * this.tileX - 0.5, this.canvas.height);
 	}
 	this.context.stroke();
-}
+};
 Graphic.prototype.drawPoint = function (x, y, text){
 	this.context.save();
 	this.context.font = "24pt Arial";
@@ -63,6 +69,35 @@ Graphic.prototype.drawPoint = function (x, y, text){
 	this.context.textBaseline = "middle";
 	this.context.fillText(text, this.tileX * x + this.tileX/2, this.tileY * y + this.tileY/2);
 	this.context.restore();
+};
+Graphic.prototype.drawBox = function (x, y, color){
+	this.context.save();
+	this.context.fillStyle = color;
+	this.context.fillRect(this.tileX * x, this.tileY * y, this.tileX, this.tileY);
+	this.context.restore();
+};
+var graph;
+function Graph(gameGrid){
+	this.grid = new Array(gameGrid.length);
+	for(var j =0; j < gameGrid.length; j++){
+		this.grid[j] = new Array(gameGrid[j].length);
+		for(var i = 0;i < gameGrid[j].length; i++){
+			switch(gameGrid[j][i]){
+				case 2:
+					this.grid[j][i] = {custo : 3, block : false, cor : "#BBBBBB"};
+					break;
+				case 1:
+					this.grid[j][i] = {custo : 2, block : false, cor : "#DDDDDD"};
+					break;
+				case 0:
+					this.grid[j][i] = {custo : 1, block : false, cor : "white"};
+					break;
+				case -1:
+					this.grid[j][i] = {custo : -1,block : true, cor : "black"}
+					break;
+			}
+		}
+	}
 }
 var graphic;
 
@@ -70,6 +105,7 @@ function init(){
 	var botao = document.getElementById("run");
 	botao.onclick = runPathFinding;
 	graphic = new Graphic(document.getElementById("grid"),mapa[0].length,mapa.length);
+	graph = new Graph(mapa);
 	graphic.render();
 	delete init;
 }
