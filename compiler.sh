df11cd4c2aa69c1e2c8f2f21e7b934b3b77f91a6
Emiliano Carlos de Moraes Firmino <elmiliox@gmail.com>
2010-08-09 10:02:13 -0400

    compiler.sh editado para melhor legibilidade

--- compiler.sh
+++ compiler.sh
@@ -1,15 +1,40 @@
 #!/bin/sh
 #Une os arquivos com o Google Closure Compiler removendo os espaços em branco
 echo "etapa 1:unificando scripts"
-java -jar /home/mangaka/jscompiler/closure_compiler.jar --compilation_level=WHITESPACE_ONLY --js=js/src/data/maps.js  --js=js/src/class/Pathfinding/GenericSearchPath.js --js=js/src/class/Pathfinding/BreadthFirstSearchPath.js --js=js/src/class/Pathfinding/aStarGenericSearchPath.js --js=js/src/class/Pathfinding/UniformCostSearchPath.js --js=js/src/class/Pathfinding/BestFirstSearchPath.js --js=js/src/class/Pathfinding/aStarDiagonalDistanceSearchPath.js --js=js/src/class/Pathfinding/aStarEuclideanDistanceSearchPath.js --js=js/src/class/Pathfinding/aStarManhattanDistanceSearchPath.js --js=js/src/class/Pathfinding/UniformCostSearchIPath.js --js=js/src/class/Pathfinding/BestFirstISearchPath.js --js=js/src/class/Pathfinding/aStarDiagonalDistanceISearchPath.js --js=js/src/class/Pathfinding/aStarEuclideanDistanceISearchPath.js --js=js/src/class/Pathfinding/aStarManhattanDistanceISearchPath.js --js=js/src/class/Graph.js --js=js/src/class/Graphic.js --js=js/src/main.js --js_output_file=js/pathfinding_ws.js
+java -jar /home/mangaka/jscompiler/closure_compiler.jar \
+  --compilation_level=WHITESPACE_ONLY \
+  --js=js/src/data/maps.js \
+  --js=js/src/class/Pathfinding/GenericSearchPath.js \
+  --js=js/src/class/Pathfinding/BreadthFirstSearchPath.js \
+  --js=js/src/class/Pathfinding/aStarGenericSearchPath.js \
+  --js=js/src/class/Pathfinding/UniformCostSearchPath.js \
+  --js=js/src/class/Pathfinding/BestFirstSearchPath.js \
+  --js=js/src/class/Pathfinding/aStarDiagonalDistanceSearchPath.js \
+  --js=js/src/class/Pathfinding/aStarEuclideanDistanceSearchPath.js \
+  --js=js/src/class/Pathfinding/aStarManhattanDistanceSearchPath.js \
+  --js=js/src/class/Pathfinding/UniformCostSearchIPath.js \
+  --js=js/src/class/Pathfinding/BestFirstISearchPath.js \
+  --js=js/src/class/Pathfinding/aStarDiagonalDistanceISearchPath.js \
+  --js=js/src/class/Pathfinding/aStarEuclideanDistanceISearchPath.js \
+  --js=js/src/class/Pathfinding/aStarManhattanDistanceISearchPath.js \
+  --js=js/src/class/Graph.js \
+  --js=js/src/class/Graphic.js \
+  --js=js/src/main.js \
+  --js_output_file=js/pathfinding_ws.js
+
 echo "ok"
 #Compila com Closure Compiler de fato
 echo "etapa 2:compilando com Google Closure Compiler"
-java -jar /home/mangaka/jscompiler/closure_compiler.jar --compilation_level=SIMPLE_OPTIMIZATIONS --js=js/pathfinding_ws.js --js_output_file=js/closure/pathfinding.js
+java -jar /home/mangaka/jscompiler/closure_compiler.jar \
+  --compilation_level=SIMPLE_OPTIMIZATIONS \
+  --js=js/pathfinding_ws.js \
+  --js_output_file=js/closure/pathfinding.js
 echo "ok"
 #Compila a versão com Yui Compressor
 echo "etapa 3: compilando com Yui Compressor"
-java -jar /home/mangaka/jscompiler/yuicompressor-2.4.2.jar js/pathfinding_ws.js -o js/yui/pathfinding.js
+java -jar /home/mangaka/jscompiler/yuicompressor-2.4.2.jar \
+  js/pathfinding_ws.js \
+  -o js/yui/pathfinding.js
 #Deleto arquivo desnecessário
 echo "ok,compilação completa"
 echo "etapa final:removendo arquivos temporarios"
